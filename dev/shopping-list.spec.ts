6247550e26de2878f22b13691087acddfeb1cfcd
bx5a <bx5a.develop@gmail.com>
2016-07-22 11:18:41 +0200

    Integrate karma for automtic test execution

--- dev/shopping-list.spec.ts
+++ dev/shopping-list.spec.ts
@@ -5,12 +5,14 @@ import {
     beforeEach} from '@angular/core/testing';
 import { ShoppingListComponent } from './shopping-list.component';
 
-describe('Shopping list', () => {
-  let list: ShoppingListComponent;
-  beforeEach(() => {
-    list = new ShoppingListComponent();
+export function main() {
+  describe('Shopping list', () => {
+    let list: ShoppingListComponent;
+    beforeEach(() => {
+      list = new ShoppingListComponent();
+    });
+    it('has name', () => {
+      expect(list.shoppingListItems[0].name).toEqual("Milk");
+    });
   });
-  it('has name', () => {
-    expect(list.shoppingListItems[0].name).toEqual("Milk");
-  });
-});
+}
